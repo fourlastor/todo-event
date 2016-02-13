db735110877bea54fb211699dcd0daa38409ee36
Daniele Conti <dnlcnt@gmail.com>
2016-02-13 12:51:30 +0000

    initial commit

---
+++ app/src/main/java/com/example/daniele/todos/TodosActivity.java
@@ -0,0 +1,15 @@
+package com.example.daniele.todos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.daniele.todoevent.R;
+
+public class TodosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todos);
+    }
+}
